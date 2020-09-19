8da568968b7fe8e5623a954805491c4ef54ffb3f
kaisei <haraguchi0209@gmail.com>
2020-09-19 15:17:14 +0900

    新規登録、ログイン、エラーメッセージカスタマイズ完了しました！

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,18 @@ module ApplicationHelper
       page_title + '|' + base_title
     end
   end
+  # デバイスのエラーメッセージ出力メソッド(新規登録)
+  def devise_error_messages
+    return "" if resource.errors.empty?
+    html = ""
+    # エラーメッセージ用のHTMLを生成
+    messages = resource.errors.full_messages.each do |msg|
+      html += <<-EOF
+        <div class="error_field alert alert-danger" role="alert">
+          <p class="error_msg">#{msg}</p>
+        </div>
+      EOF
+    end
+    html.html_safe
+  end
 end
--- db/migrate/20200914221605_devise_create_users.rb
+++ db/migrate/20200914221605_devise_create_users.rb
@@ -34,6 +34,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
 
       t.timestamps null: false
+
+      #追加属性
+      t.string :name, null: false
     end
 
     add_index :users, :email,                unique: true
