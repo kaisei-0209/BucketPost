b8eb0a14edff8ebf00ed1a3f3015aec3152c32c6
kaisei <haraguchi0209@gmail.com>
2020-10-01 22:29:51 +0900

    Userモデルにadminを追加#5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  # before_action :admin_user,     only: :destroy
 
 
   def show
     @user = User.find(params[:id])
   end
+
+  # private
+  #  # 管理者かどうか確認
+  #  def admin_user
+  #   redirect_to(root_url) unless current_user.admin?
+  # end
+
 end
