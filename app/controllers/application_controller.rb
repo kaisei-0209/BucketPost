417a04b5d74f26c665c4e6bf68ab6b0aaa5dcaf0
kaisei <haraguchi0209@gmail.com>
2020-10-09 21:03:50 +0900

    carrierwaveとimageカラム追加#8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
 protected
   def configure_permitted_parameters
-    # サインアップ時にnameのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    # アカウント編集の時にnameとprofileのストロングパラメータを追加
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+    # サインアップ時にname,profileのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile])
+    # アカウント編集の時にname,image,profileのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :profile])
   end
 end
