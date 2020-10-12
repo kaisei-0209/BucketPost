ba2487c1a582929c0bf3985a842c5f4632a4e5c0
kaisei <haraguchi0209@gmail.com>
2020-10-12 23:56:36 +0900

    bulmaを正常動作

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 protected
+
+def after_sign_in_path_for(resource)
+  root_path
+end
+
   def configure_permitted_parameters
     # サインアップ時にname,profileのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile])
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,11 +4,11 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 // 追加
-require("bootstrap/dist/js/bootstrap")
+// require("bootstrap/dist/js/bootstrap")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
