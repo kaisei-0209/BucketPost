28727fb8e258838371722e77e94de2019b355bbf
kaisei <haraguchi0209@gmail.com>
2020-11-21 16:27:26 +0900

    Commentモデルの存在性と、文字数の検証をしました#55

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    content { "MyString" }
-    user { nil }
-    post { nil }
+    content { "コメント" }
+    association :post
+    user { post.user }
   end
 end
