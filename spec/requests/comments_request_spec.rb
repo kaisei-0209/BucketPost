a11e4a1d727466c442bc9eb62ab723889b532c4c
kaisei <haraguchi0209@gmail.com>
2020-11-23 23:15:57 +0900

    コメント機能のテストをしました

--- spec/requests/comments_request_spec.rb
+++ spec/requests/comments_request_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
+  let!(:user) { create(:user) }
+  let!(:post) { create(:post, user: user) }
 
+  describe "#create" do
+    context "パラメータが揃っている場合" do
+      it "正常に登録できること" do
+        content = build(:comment, content: "応援します！", post: post)
+        expect { content.save }.to change { post.comments.count }.by(1)
+      end
+    end
+
+    context "パラメータが揃っていない場合" do
+      it "登録できないこと" do
+        content = build(:comment, content: "", post: post)
+        expect { content.save }.to change { post.comments.count }.by(0)
+      end
+    end
+  end
+
+  describe "#destroy" do
+    it "正常に削除できること" do
+      content = create(:comment, post: post)
+      expect { content.destroy }.to change { post.comments.count }.by(-1)
+    end
+  end
 end
