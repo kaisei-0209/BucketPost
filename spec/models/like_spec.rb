2d935293ded45af1b5be39a03ddf3610b7fb1915
kaisei <haraguchi0209@gmail.com>
2020-11-25 22:52:21 +0900

    いいね機能のテストをしました

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
+  let!(:like) { create(:like, user: user, post: post) }
+
+    it "投稿とユーザーが存在すれば保存されること" do
+      expect(like).to be_valid
+    end
+
+    it "一つの投稿に同じユーザーが2回以上保存できないこと" do
+      like = build(:like, user: user, post: post)
+      like.valid?
+      expect(like).to_not be_valid
+    end
+
+    it "投稿がない場合は保存できないこと" do
+      like = build(:like, post: nil)
+      like.valid?
+      expect(like).to_not be_valid
+    end
+
+    it "ユーザーがいない場合は保存されないこと" do
+      like = build(:like, user: nil)
+      like.valid?
+      expect(like).to_not be_valid
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -65,5 +65,4 @@ RSpec.configure do |config|
 
   #Deviseのテストヘルパーを使用する
   config.include Devise::Test::IntegrationHelpers, type: :request
-
 end
