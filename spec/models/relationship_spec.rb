9c58b49d9c89ba0942089a9e493fe37c308c14cd
kaisei <haraguchi0209@gmail.com>
2020-11-25 12:40:23 +0900

    フォロー機能のテストをしました

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    let(:follower) { create(:user) }
+    let(:following) { create(:user) }
+    let!(:relationship) { create(:relationship, follower: follower, following: following) }
+
+    it "followerとfollowingが存在すれば保存されること" do
+      expect(relationship).to be_valid
+    end
+
+    it "follower_idがなければ保存できない" do
+      relationship.follower_id = nil
+      expect(relationship).to_not be_valid
+    end
+
+    it "followed_idがなければ作成できない" do
+      relationship.following_id = nil
+      expect(relationship).to_not be_valid
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -134,7 +134,4 @@ RSpec.describe User, type: :model do
     end
   end
 
-
-
-
 end
